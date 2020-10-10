818824eccc58b2fbb8b4ba12b1c548009431d31e
brittanymclaughlin <britt@brittanytroutman.com>
2020-10-09 22:47:00 -0400

    MVP completed

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,70 @@
 const express = require('express');
-
+const postDb = require('./postDb');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  // do your magic!
+  postDb.get(req.query)
+  .then((post)=>{
+    res.status(200).json(post)
+  })
+  .catch((error)=> {
+    next(error)
+  })
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get('/:id', validatePostId, (req, res) => {
+  postDb.getById(req.params.id)
+  .then((post)=>{
+    if(post){
+      res.status(200).json(post)
+    }
+  })
+  .catch((error)=>{
+    next(error)
+  })
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+router.delete('/:id', validatePostId, (req, res) => {
+  postDb.remove(req.params.id)
+  .then((post)=> {
+    res.status(200).json({message: "Post has been terminated"})
+  })
+  .catch((error)=> {
+    next(error)
+  })
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put('/:id', validatePostId, (req, res) => {
+  postDb.update(req.params.id, req.body)
+  .then((post)=> {
+    if(post){
+      //changed
+      const text = req.body.text
+      res.status(201).json({message: "Post was updated successfully", text, post})
+    }
+    
+  })
+  .catch((error)=>{
+    next(error)
+  })
 });
 
 // custom middleware
-
 function validatePostId(req, res, next) {
   // do your magic!
+  postDb.getById(req.params.id)
+  .then((posts)=> {
+    if(posts){
+      req.post = posts
+      next()
+    }else{
+      res.status(400).json({message: "invalid post id"})
+    }
+    
+  })
+  .catch((error)=>{
+    next(error)
+  })
 }
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,23 @@
+require('dotenv').config();
 const express = require('express');
-
+const helmet = require('helmet');
+const usersRouter = require('./users/userRouter.js');
+const postRouter = require('./posts/postRouter');
 const server = express();
+
+server.use(helmet())
+server.use(logger());
+server.use(express.json());
+server.use('/api/users', usersRouter);
+
+server.use((err, req, res, next) => {
+	console.log(err)
+	
+	res.status(500).json({
+		message: "Something went wrong, please try again later",
+	})
+})
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +25,11 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger() {
+  return function (req, res, next) {
+    console.log( ` A ${req.method} has been made to ${req.url}`)
+    next()
+  }
+}
 
 module.exports = server;
